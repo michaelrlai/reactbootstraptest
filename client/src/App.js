a6cffdff3f622bccd82418532284f76e56febeca
Michael Lai <mrlai7782@gmail.com>
2021-06-09 10:05:33 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./App.scss";
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+function App() {
+  return (
+    <div className="">
+      <Button variant="primary">Primary</Button>
+    </div>
+  );
+}
+
+export default App;
